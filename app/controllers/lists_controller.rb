9df42b8dbaa8a9693993757fd4ec632e845b351f
sowmya dinavahi <sowmya.dsl@gmail.com>
2017-06-26 14:01:54 -0700

    Add delete function and create a error template and form template and reformat the code

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -23,6 +23,26 @@ class ListsController < ApplicationController
     end
   end
 
+    def edit
+      @list = List.find(params[:id])
+      render :edit
+    end
+
+    def update
+      @list = List.find(params[:id])
+      if @list.update(list_params)
+        redirect_to lists_path
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @list = List.find(params[:id])
+      @list.destroy
+      redirect_to lists_path
+    end
+
     private
     def list_params
       params.require(:list).permit(:name)

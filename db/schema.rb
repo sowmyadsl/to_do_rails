4e6965857f86c2edcdac76829bad7b0b681380a7
sowmya dinavahi <sowmya.dsl@gmail.com>
2017-06-26 16:02:45 -0700

    create a task object and add relationship to lists and added crud functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170626192439) do
+ActiveRecord::Schema.define(version: 20170626214932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "description"
+    t.integer "list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

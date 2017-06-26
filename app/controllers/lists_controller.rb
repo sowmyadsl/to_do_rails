d70891cd86bc72a195aaea2c9f598fb4b1c69358
sowmya dinavahi <sowmya.dsl@gmail.com>
2017-06-26 13:26:42 -0700

    add actions to create new lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -15,5 +15,16 @@ class ListsController < ApplicationController
   end
 
   def create
-    
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
+    private
+    def list_params
+      params.require(:list).permit(:name)
+    end
 end
